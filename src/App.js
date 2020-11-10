5aba7fd5d33339f7b9744cf591dc107fc0c9afdd
jztimm <jztimm@gmail.com>
2020-11-09 22:13:02 -0500

    Get Google Maps loaded and added a custom color scheme to it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import { formatRelative } from "date-fns";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "@reach/combobox/styles.css"
+import mapStyles from './mapStyles'
+
+
+
+
+
+
+
+
+// function Map() {
+//   return (
+//     <GoogleMap
+//       defaultZoom={10}
+//       defaultCenter={{ lat: 40.844784, lng: -73.864830 }}
+//     />
+//   );
+// }
+
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+const center = {
+  lat: 40.844784,
+  lng: -73.864830
+};
+const options = {
+  styles: mapStyles
 }
 
-export default App;
+
+export default function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  })
+
+  if (loadError) return "Error loading maps";
+  if(!isLoaded) return "Loading Maps";
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        ></GoogleMap>
+    </div>)
+  ;
+}
